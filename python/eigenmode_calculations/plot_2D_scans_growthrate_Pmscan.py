0d3e1a8706fbf45a9336c67821b7ff121b545b7e
Adrian (macbook) <adfraser@ucsc.edu>
2022-08-31 10:02:59 -0700

    Production plot_2D_scans_growthrate_Pmscan.py

--- python/eigenmode_calculations/plot_2D_scans_growthrate_Pmscan.py
+++ python/eigenmode_calculations/plot_2D_scans_growthrate_Pmscan.py
@@ -21,7 +21,7 @@ out_file1 = out_dir1 / 'set_{}.h5'.format(set_num1)
 out_file2 = out_dir2 / 'set_{}.h5'.format(set_num2)
 out_file3 = out_dir3 / 'set_{}.h5'.format(set_num3)
 
-plot_fname = 'plots/growthrate_2dscan_Pmscan.pdf'
+plot_fname = 'plots/growthrate_2dscan_Pmscan.eps'
 
 with h5py.File(out_file1, mode='r') as file:
     ks1 = np.array(file['scan_values/ks'])
@@ -60,52 +60,31 @@ with h5py.File(out_file3, mode='r') as file:
     contains_ordinary_mode3 = np.array(file['results/contains_ordinary_mode'])
 
 scale = 0.6
-# plt.figure(figsize=(scale*21, scale*5))
-# plt.subplot(1, 3, 1)
 fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(scale*21, scale*5))
 ax1, ax2, ax3 = axes.flat
 im = ax1.contourf(Res1, HBs1, -np.imag(omegas1.T), levels=np.geomspace(1e-5, 1e0, 11), norm=colors.LogNorm())
-# ax1.contourf(Res1, HBs1, np.abs(np.real(omegas1)).T, [0.0, 1e-10], hatches=['/', None], alpha=0)
-# ax1.contour(Res1, HBs1, np.abs(np.real(omegas1)).T, [0.0, 1e-10], colors='k', linewidths=2.0)
 ax1.contourf(Res1, HBs1, contains_ordinary_mode1.astype(int).T, [-1, 0, 1], hatches=[None, '\\', None], alpha=0)
 ax1.contour(Res1, HBs1, contains_ordinary_mode1.astype(int).T, [0, 1], colors='k', linewidths=2.0)
 ax1.contourf(Res1, HBs1, contains_strange_mode1.astype(int).T, [-1, 0, 1], hatches=[None, '/', None], alpha=0)
 ax1.contour(Res1, HBs1, contains_strange_mode1.astype(int).T, [0, 1], colors='k', linewidths=2.0)
 ax1.plot([10, 500, 10, 500], [0.2, 0.2, 1.0, 1.0], 'd', c='magenta')
 ax1.set_ylabel(r'$C_B$')
-# plt.xlabel(r'$\mathrm{Re}$')
-# ax1.set_title(r'$\mathrm{{Re}}[\lambda]$ for $Pm = {:3.1f}$'.format(Pm1))
 ax1.set_title(r'$Pm = {:3.1f}$'.format(Pm1))
 
-# plt.subplot(1, 3, 2)
 im = ax2.contourf(Res2, HBs2, -np.imag(omegas2.T), levels=np.geomspace(1e-5, 1e0, 11), norm=colors.LogNorm())
-# ax2.contourf(Res2, HBs2, np.abs(np.real(omegas2)).T, [0.0, 1e-10], hatches=['/', None], alpha=0)
-# ax2.contour(Res2, HBs2, np.abs(np.real(omegas2)).T, [0.0, 1e-10], colors='k', linewidths=2.0)
 ax2.contourf(Res2, HBs2, contains_ordinary_mode2.astype(int).T, [-1, 0, 1], hatches=[None, '\\', None], alpha=0)
 ax2.contour(Res2, HBs2, contains_ordinary_mode2.astype(int).T, [0, 1], colors='k', linewidths=2.0)
 ax2.contourf(Res2, HBs2, contains_strange_mode2.astype(int).T, [-1, 0, 1], hatches=[None, '/', None], alpha=0)
 ax2.contour(Res2, HBs2, contains_strange_mode2.astype(int).T, [0, 1], colors='k', linewidths=2.0)
-# ax2.xlabel(r'$\mathrm{Re}$')
-# ax2.set_title(r'$\mathrm{{Re}}[\lambda]$ for $Pm = {:3.1f}$'.format(Pm2))
 ax2.set_title(r'$Pm = {:3.1f}$'.format(Pm2))
 
-# plt.subplot(1, 3, 3)
 im = ax3.contourf(Res3, HBs3, -np.imag(omegas3.T), levels=np.geomspace(1e-5, 1e0, 11), norm=colors.LogNorm())
 fig.colorbar(im, ax=axes.ravel().tolist(), label=r'$\mathrm{Re}[\lambda]$')
-# ax3.contourf(Res3, HBs3, np.abs(np.real(omegas3)).T, [0.0, 1e-10], hatches=['/', None], alpha=0)
-# ax3.contour(Res3, HBs3, np.abs(np.real(omegas3)).T, [0.0, 1e-10], colors='k', linewidths=2.0)
 ax3.contourf(Res3, HBs3, contains_ordinary_mode3.astype(int).T, [-1, 0, 1], hatches=[None, '\\', None], alpha=0)
 ax3.contour(Res3, HBs3, contains_ordinary_mode3.astype(int).T, [0, 1], colors='k', linewidths=2.0)
 ax3.contourf(Res3, HBs3, contains_strange_mode3.astype(int).T, [-1, 0, 1], hatches=[None, '/', None], alpha=0)
 ax3.contour(Res3, HBs3, contains_strange_mode3.astype(int).T, [0, 1], colors='k', linewidths=2.0)
 ax3.plot([10, 500, 500], [0.2, 0.2, 0.6], 'd', c='magenta')
-# re2 = 1e3
-# cb2 = 6e-1  # cb1*(re1/re2)**2.0
-# cb1 = 1e0  # cb2*(re2/re1)**2.0
-# re1 = np.sqrt(cb2/cb1)*re2
-# ax3.plot([re1, re2], [cb1, cb2], '--', c='orange')
-# ax3.xlabel(r'$\mathrm{Re}$')
-# ax3.set_title(r'$\mathrm{{Re}}[\lambda]$ for $Pm = {:3.1f}$'.format(Pm3))
 ax3.set_title(r'$Pm = {:3.1f}$'.format(Pm3))
 
 for ax in axes.flat:
@@ -120,5 +99,5 @@ for ax in [ax2, ax3]:
 
 ax1.axvline(np.sqrt(2*1.1/(0.1*0.9)), c='r')
 
-plt.show()
-# plt.savefig(plot_fname, bbox_inches='tight')
+# plt.show()
+plt.savefig(plot_fname, bbox_inches='tight')
